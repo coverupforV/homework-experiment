81b58b160ad20899667ede4e69376c31d53ab575
coverupforV <pcw760404931@163.com>
2023-04-10 20:11:08 +0800

    modified:   os/os-ex2-4.cpp 	modified:   os/os-ex2-4.exe

--- os/os-ex2-4.cpp
+++ os/os-ex2-4.cpp
@@ -12,7 +12,7 @@ void producer(sbuf* p) {
     int item;
     uniform_int_distribution<unsigned> k(500, 1500);
     static uniform_int_distribution<unsigned> u(1, 100);
-    static default_random_engine e(time(0));  //随机数引擎
+    static default_random_engine e(time(0)%3000);  //随机数引擎
     for (int i = 0; i < 20; i++) {     //生产20次
         item = u(e);
         p->insert(item);
@@ -23,7 +23,7 @@ void producer(sbuf* p) {
 //消费者线程
 void consumer(sbuf* p) {
     uniform_int_distribution<unsigned> k(1000, 2000);
-    default_random_engine v(time(0));    //随机数引擎
+    default_random_engine v(time(0)%2000);    //随机数引擎
     for (int i = 0; i < 20; i++) {     //消费20次
         p->remove();
         Sleep(k(v));
